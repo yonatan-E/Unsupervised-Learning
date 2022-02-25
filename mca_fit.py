572eb030ed72f387c4f740fe158382443e31a8a3
yonatan-E <yonatan16.er@gmail.com>
2022-02-25 15:36:08 +0200

    .

---
+++ mca_fit.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from prince.mca import MCA
+import matplotlib.pyplot as plt
+import seaborn as sns
+import logging, sys
+
+from constants import EXTERNAL_FEATURES
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(asctime)s - %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
+
+df = pd.read_csv('data/census-data.csv').sample(20000).drop(EXTERNAL_FEATURES + ['caseid'], axis=1)
+
+inertias = []
+
+for n in range(2, 65):
+    logging.info(f'Running MCA for {n} components')
+
+    mca = MCA(n_components=n, random_state=0)
+    mca.fit(df)
+    inertias.append(mca.total_inertia_)
+
+sns.lineplot(x=range(2, 65), y=inertias)
+plt.show()
