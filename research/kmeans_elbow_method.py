d653f7f89a446f891005d5c376b583dc4b765ebd
yonatan-E <yonatan16.er@gmail.com>
2022-02-25 16:17:36 +0200

    .

--- research/kmeans_elbow_method.py
+++ research/kmeans_elbow_method.py
@@ -3,20 +3,25 @@ from yellowbrick.cluster import KElbowVisualizer
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+from prince.mca import MCA
 
 EXTERNAL_FEATURES = ['dAge', 'dHispanic', 'iYearwrk', 'iSex']
 
 df = pd.read_csv('data/census-data.csv').drop(EXTERNAL_FEATURES + ['caseid'], axis=1)
 
-X = df.sample(20000).values
+mca = MCA(n_components=20, random_state=0)
+
+X = mca.fit_transform(df.sample(20000)).values
 
 losses = []
-for k in range(2, 16):
+for k in range(2, 20):
     model = KMeans(n_clusters=k)
     model.fit(X)
     losses.append(model.inertia_)
 
-plt.plot(losses, color='blue', marker='o')
+sns.set_theme(style="darkgrid")
+sns.lineplot(x=range(2, 20), y=losses)
 plt.grid(axis='both', alpha=.3)
 plt.xticks(fontsize=7, alpha=.7)
 plt.yticks(fontsize=7, alpha=.7)
