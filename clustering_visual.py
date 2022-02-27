ca60840d85043d8bf28eebb0b1f40a0046fed3bb
yonatan-E <yonatan16.er@gmail.com>
2022-02-27 17:56:41 +0200

    .

--- clustering_visual.py
+++ clustering_visual.py
@@ -7,6 +7,7 @@ from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 from prince.mca import MCA
 from sklearn.preprocessing import OneHotEncoder
+import random, time
 
 from utils import plot_clusters
 from constants import *
@@ -16,23 +17,23 @@ warnings.filterwarnings('ignore', category=FutureWarning)
 warnings.filterwarnings('ignore', category=UserWarning)
 
 MODELS = [
-    #GaussianMixture(n_components=3),
-    #KMeans(n_clusters=3),
-    #KMeans(n_clusters=4),
-    #KMeans(n_clusters=5),
-    #KMeans(n_clusters=6)
-    DBSCAN(eps=4.00, min_samples=650),
-    DBSCAN(eps=4.05, min_samples=650),
-    DBSCAN(eps=4.10, min_samples=650),
-    DBSCAN(eps=4.15, min_samples=650),
-    DBSCAN(eps=4.20, min_samples=650),
-    DBSCAN(eps=4.25, min_samples=650),
-    DBSCAN(eps=4.30, min_samples=650),
-    DBSCAN(eps=4.35, min_samples=650),
-    DBSCAN(eps=4.40, min_samples=650),
-    DBSCAN(eps=4.45, min_samples=650),
-    DBSCAN(eps=4.50, min_samples=650),
-    DBSCAN(eps=4.55, min_samples=650),
+    GaussianMixture(n_components=3),
+    KMeans(n_clusters=3),
+    KMeans(n_clusters=4),
+    KMeans(n_clusters=5),
+    KMeans(n_clusters=6),
+    DBSCAN(eps=4.00, min_samples=1500),
+    DBSCAN(eps=4.05, min_samples=1500),
+    DBSCAN(eps=4.10, min_samples=1500),
+    DBSCAN(eps=4.15, min_samples=1500),
+    DBSCAN(eps=4.20, min_samples=1500),
+    DBSCAN(eps=4.25, min_samples=1500),
+    DBSCAN(eps=4.30, min_samples=1500),
+    DBSCAN(eps=4.35, min_samples=1500),
+    DBSCAN(eps=4.40, min_samples=1500),
+    DBSCAN(eps=4.45, min_samples=1500),
+    DBSCAN(eps=4.50, min_samples=1500),
+    DBSCAN(eps=4.55, min_samples=1500),
     #SpectralClustering(n_clusters=2, affinity='nearest_neighbors', random_state=0),
     #SpectralClustering(n_clusters=3, affinity='nearest_neighbors', random_state=0),
     #AgglomerativeClustering(n_clusters=3),
@@ -41,9 +42,11 @@ MODELS = [
 ]
 
 if __name__ == '__main__':
+    np.random.seed(int(time.time()))
+
     df = pd.read_csv('data/census-data.csv').drop(EXTERNAL_FEATURES + ['caseid'], axis=1)
     enc = OneHotEncoder()
-    X = enc.fit_transform(df.sample(SAMPLE_SIZE)).toarray()
+    X = enc.fit_transform(df.sample(100000)).toarray()
 
     embedder = TSNE(n_components=2, perplexity=30)
     #embedder = MCA(n_components=2)
