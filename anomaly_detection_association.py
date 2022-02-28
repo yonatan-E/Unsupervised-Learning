0ab37a4c5d0b833c405df78f132cb0498efe530c
Itay Gradenwits <itay25433@gmail.com>
2022-03-01 00:04:28 +0200

    run a script that fins associations between External and anoamly and got those results.

--- anomaly_detection_association.py
+++ anomaly_detection_association.py
@@ -5,6 +5,7 @@ from sklearn.metrics import mutual_info_score
 from sklearn.preprocessing import OneHotEncoder
 import numpy as np
 import logging, sys, time
+from sklearn.svm import OneClassSVM
 
 from utils import perform_statistical_tests
 from constants import *
@@ -19,24 +20,30 @@ np.random.seed(int(time.time()))
 
 NUM_ITERATIONS = 20
 
+MODELS = [
+    DBSCAN(eps=5.5, min_samples=720),
+    OneClassSVM(kernel='rbf', gamma='scale', nu=0.1),
+]
+
 
 if __name__ == '__main__':
     df = pd.read_csv('data/census-data.csv')
 
     features_mutual_info = {feat: pd.DataFrame() for feat in EXTERNAL_FEATURES}
 
+    enc = OneHotEncoder()
+
     for _ in range(NUM_ITERATIONS):
         logging.info(f'Running iteration {_}')
 
         sample_df = df.sample(SAMPLE_SIZE)
-        anomalies_1 = find_anomalies_1(sample_df.drop(EXTERNAL_FEATURES + ['caseid'], axis=1))
-        anomalies_2 = find_anomalies_2(sample_df.drop(EXTERNAL_FEATURES + ['caseid'], axis=1))
-
-
+        X = enc.fit_transform(sample_df.drop(EXTERNAL_FEATURES + ['caseid'], axis=1)).toarray()
+        anomalies_1 = [1 if l == -1 else 0 for l in MODELS[0].fit_predict(X)]
+        anomalies_2 = [1 if l ==-1 else 0 for l in MODELS[1].fit_predict(X)] 
         for feat in EXTERNAL_FEATURES:
             features_mutual_info[feat] = features_mutual_info[feat].append({
-                'method_1': mutual_info_score(sample_df[feat], anomalies_1),
-                'method_2': mutual_info_score(sample_df[feat], anomalies_1)
+                'DBSCAN': mutual_info_score(sample_df[feat], anomalies_1),
+                'OneClassSVM': mutual_info_score(sample_df[feat], anomalies_2)
             }, ignore_index=True)
 
     if len(sys.argv) > 1 and sys.argv[1] == '--save':
