b79ec1d8f7314c694757da89e3fd415655122dc5
ilandovprais <ilandovprais@gmail.com>
2022-03-02 17:35:37 +0200

    .

--- clustering_association_visual.py
+++ clustering_association_visual.py
@@ -9,8 +9,13 @@ from scipy import interpolate
 from scipy.spatial import ConvexHull, convex_hull_plot_2d
 import matplotlib.pyplot as plt
 import seaborn as sns
-import time
+import sys, time
 
+assert len(sys.argv) > 1
+
+dataset = sys.argv[1]
+
+from utils import encode_mixed_data
 from constants import *
 
 np.random.seed(int(time.time()))
@@ -35,25 +40,44 @@ def plot_clustering_association(points, labels, feature, axe):
     legend = axe.legend(*scatter.legend_elements(), loc='upper right', title='Class')
     axe.add_artist(legend)
 
-df = pd.read_csv('data/census.csv')
-sample_df = df.sample(SAMPLE_SIZE)
+if dataset == 'census':
+    df = pd.read_csv('data/census.csv')
+    sample_df = df.sample(SAMPLE_SIZE)
 
-enc = OneHotEncoder()
-X = enc.fit_transform(sample_df.drop(EXTERNAL_CENSUS_FEATURES + ['caseid'], axis=1)).toarray()
+    X = OneHotEncoder().fit_transform(sample_df.drop(EXTERNAL_CENSUS_FEATURES + ['caseid'], axis=1)).toarray()
 
-embedder = TSNE(n_components=2, perplexity=60)
-#embedder = MCA(n_components=2)
-points = embedder.fit_transform(X)
+    embedder = TSNE(n_components=2, perplexity=60)
+    points = embedder.fit_transform(X)
 
-fig, axs = plt.subplots(1, 2, figsize=(15, 5))
+    fig, axs = plt.subplots(1, 2, figsize=(15, 5))
 
-model = KMeans(n_clusters=4)
-labels = model.fit_predict(X)
+    model = GaussianMixture(n_components=3)
+    labels = model.fit_predict(X)
 
-plot_clustering_association(points, labels, sample_df['dAge'], axs[0])
-axs[0].set_title('K-Means clusters association with dAge external feature')
+    plot_clustering_association(points, labels, sample_df['dAge'], axs[0])
+    axs[0].set_title('K-Means clusters association with dAge external feature')
 
-plot_clustering_association(points, labels, sample_df['iYearwrk'], axs[1])
-axs[1].set_title('K-Means clusters association with iYearwrk external feature')
+    plot_clustering_association(points, labels, sample_df['iYearwrk'], axs[1])
+    axs[1].set_title('K-Means clusters association with iYearwrk external feature')
 
-plt.savefig('plots/clustering_dAge_iYearwrk_association.png')
+    plt.savefig(f'plots/{dataset}/clustering_dAge_iYearwrk_association.png')
+
+if dataset == 'shoppers':
+    df = pd.read_csv('data/online-shoppers-intention.csv').astype(SHOPPERS_DATA_TYPES)
+
+    X = encode_mixed_data(df.drop(EXTERNAL_SHOPPERS_FEATURES, axis=1))
+
+    embedder = TSNE(n_components=2, perplexity=30)
+    points = embedder.fit_transform(X)
+
+    axe = plt.gca()
+
+    model = GaussianMixture(n_components=3)
+    labels = model.fit_predict(X)
+
+    plot_clustering_association(points, labels, df['Revenue'], axe)
+    axe.set_title('Gaussian Mixture Model clusters association with Revenue external feature')
+
+    # plt.show(f'plots/{dataset}/clustering_Revenue_association.png')
+    # plt.show(f'plots/{dataset}/clustering_Revenue_association.svg')
+    plt.show()
